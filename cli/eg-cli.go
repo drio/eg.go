015ac0b7173ebf6782356fad69732cf97b71a696
David Rio Deiros <driodeiros@gmail.com>
2013-03-10 11:39:35 -0500

    Adding format script. Changing format 2 spaces

--- cli/eg-cli.go
+++ cli/eg-cli.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"github.com/drio/eg.go"
-	"log"
-	"net"
+  "github.com/drio/eg.go"
+  "log"
+  "net"
 )
 
 func main() {
-	service := ":8000"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	eg.CheckError(err)
+  service := ":8000"
+  tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+  eg.CheckError(err)
 
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	eg.CheckError(err)
+  listener, err := net.ListenTCP("tcp", tcpAddr)
+  eg.CheckError(err)
 
-	log.Printf("Starting server on port %s", service)
-	n := 0 // Number of connections since we started
-	for {
-		conn, err := listener.Accept()
-		log.Printf("A new connection: %d\n", n)
-		if err != nil {
-			continue
-		}
-		go eg.HandleClient(conn, n)
-		n++
-	}
+  log.Printf("Starting server on port %s", service)
+  n := 0 // Number of connections since we started
+  for {
+    conn, err := listener.Accept()
+    log.Printf("A new connection: %d\n", n)
+    if err != nil {
+      continue
+    }
+    go eg.HandleClient(conn, n)
+    n++
+  }
 }
--- common.go
+++ common.go
@@ -1,13 +1,13 @@
 package eg
 
 import (
-	"fmt"
-	"os"
+  "fmt"
+  "os"
 )
 
 func CheckError(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
-	}
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+    os.Exit(1)
+  }
 }
